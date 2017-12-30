69b4f6af3c6b826eac171b30b1209243b79024fa
kedar700 <kedarnaik16@gmail.com>
2017-12-30 10:52:45 +0530

    Initial Commit

---
+++ src/main/java/com/joke/springjokes/SpringjokesApplication.java
@@ -0,0 +1,12 @@
+package com.joke.springjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjokesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/joke/springjokes/SpringjokesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joke.springjokes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringjokesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
